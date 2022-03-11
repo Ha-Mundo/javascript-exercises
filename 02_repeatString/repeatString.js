78854d92a12dd4d60b0a0a76044e8a67bf20bffe
Ha-Mundo <trauzzu@gmail.com>
2022-03-11 19:45:19 +0100

    exercise 02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function (str = "", num) {
+  let result = "";
 
+  if (num === -1) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < num; i++) {
+    if (num > 0) result += str;
+  }
+  return result;
 };
+
+const number = Math.floor(Math.random() * 1000);
+
+repeatString("hey", number);
 
 // Do not edit below this line
 module.exports = repeatString;
