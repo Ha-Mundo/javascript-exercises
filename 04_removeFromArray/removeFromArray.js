0e03bf1a1491ff0758d768f717f684a9be437e65
Ha-Mundo <trauzzu@gmail.com>
2022-03-13 15:27:36 +0100

    exercise 04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...args) {
+  const array = args[0];
+  return array.filter(item => !args.includes(item));
 };
 
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
 // Do not edit below this line
 module.exports = removeFromArray;
