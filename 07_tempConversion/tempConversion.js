85ede5bc5e3eb48c78a16ed86af14acb21889ce9
Ha-Mundo <trauzzu@gmail.com>
2022-03-16 16:23:39 +0100

    exercise 07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  //	[°C] = ([°F] − 32) × 5⁄9
+  let celsius = ((temp - 32) * 5) / 9;
+  let roundedCelsius = Math.round(celsius * 10) / 10;
+  return roundedCelsius;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  // [°F] = [°C] × 9⁄5 + 32
+  let fahrenheit = temp * (9 / 5) + 32;
+  let roundedfahrenheit = Math.round(fahrenheit * 10) / 10;
+  return roundedfahrenheit;
 };
+
+console.log(ftoc(-32), ctof(0));
+ftoc(-32);
+ctof(0);
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
